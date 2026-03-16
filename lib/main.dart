76cf884eec035625da67d3a10a82417ec1cfe5bf
Daba <dabaaaaa17@ya.ru>
2026-03-16 23:42:54 +0700

    initial project structure and flutter setup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Habit Tracker',
+      theme: ThemeData.dark(),
+      home: const Scaffold(
+        body: Center(child: Text('Habit Tracker стартовал!')),
+      ),
+    );
+  }
+}
